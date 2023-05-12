3d8179facc6ab9dd19fa071a0ad860d001ec0b63
Lou Jacoubs <lou.jacoubs@kellogg.northwestern.edu>
2023-05-12 16:37:07 +0000

    added @ to controller and application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    @name = ApplicationController.all
+    @content = ApplicationController.all 
+  end
+
+  def new 
+    @name = ApplicationController.new
+    @content = ApplicationController.new 
+  end
+  
+
+
 end
