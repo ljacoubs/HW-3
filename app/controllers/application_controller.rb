d78767e11098b019be00e3cfc738ef276b1b32a6
Lou Jacoubs <lou.jacoubs@kellogg.northwestern.edu>
2023-05-13 16:08:24 +0000

    working it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
 
-  def index
-    @name = ApplicationController.all
-    @content = ApplicationController.all 
-  end
-
-  def new 
-    @name = ApplicationController.new
-    @content = ApplicationController.new 
-  end
-  
-
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
 
-def index
-  render :template => "posts/index"
+
+  # def new
+  #   @place = Place.new
+  #   end
+    
+  #   def create 
+  #     @place = Place.new 
+  #     @place["name"] = params["place"]["name"]
+  #     @place.save
+  #     redirect_to "/places"
+  #   end
 
 end
+
+#in create
+# t.string "title"
+# t.text "description"
+# t.date "posted_on"
+# t.integer "place_id"
+
+
+show
